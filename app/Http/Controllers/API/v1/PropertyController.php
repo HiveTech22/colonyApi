44387d386123c17f1b70b2c1eb477d21defa76c5
HiveTech22 <hivehub22@gmail.com>
2022-05-29 10:34:39 +0100

    review property

--- app/Http/Controllers/API/v1/PropertyController.php
+++ app/Http/Controllers/API/v1/PropertyController.php
@@ -15,19 +15,46 @@ use App\Http\Resources\v1\ReviewResource;
 use App\Http\Resources\v1\PropertyResource;
 use App\Http\Resources\v1\PropertyCollection;
 
+/**
+ * @group Property Management
+ * APIs to manage property
+*/
+
 class PropertyController extends Controller
 {
 
+    /**
+     * Display a listing of properties.
+     *
+     * Gets a list of properties.
+     *
+     * @queryParam page_size int Size per page. Defaults to 5. Example: 5
+     * @queryParam page int Page to view. Example: 1
+     *
+     * @apiResourceCollection App\Http\Resources\v1\PropertyResource
+     * @apiResourceModel App\Models\Property
+     * @return ResourceCollection
+     */
     public function index(Request $request)
     {
-        $pageSize = $request->page ?? 5;
+        $pageSize = $request->page_size ?? 5;
         return new PropertyCollection(Property::query()->where('isAvailable', true)->inRandomOrder()->paginate($pageSize));
     }
 
-
-    public function store(PropertyRequest $request)
+    /**
+     * Store a newly created property in storage.
+     * @bodyParam title string required Title of the property. Example: Amazing property
+     * @bodyParam body string[] required Body of the property. Example: ["This property is super beautiful"]
+     * @bodyParam author_id int[] required The author ids of the property. Example: [1, 2]
+     * @apiResource App\Http\Resources\v1\PropertyResource
+     * @apiResourceModel App\Models\Property
+     * @param  PropertyRequest  $request
+     * @return PropertyResource
+     */
+    public function store(PropertyRequest $request, PropertyRepository $repositor)
     {
 
+        $property = $repository->create($payload);
         $property = Property::create([
             'title'             =>$request->input('title'),
             'slug'              =>Str::slug($request->input('title')),
@@ -36,14 +63,19 @@ class PropertyController extends Controller
             'latitude'          =>$request->input('latitude'),
             'image'             =>$request->input('image'),
             'description'       =>$request->input('description'),
-            'author_id'         => auth()->id() ?? 1
+            'author_id'         => auth()->id()
         ]);
         
-        return (new PropertyResource($property))
-        ->response()
-        ->setStatusCode(201);
+        return (new PropertyResource($property));
     }
 
+    /**
+     * Display the specified Property.
+     * @apiResource App\Http\Resources\v1\PropertyResource
+     * @apiResourceModel App\Models\Property
+     * @param  \App\Models\Property  $Property
+     * @return PropertyResource
+     */
     public function show(Property $property)
     {
         return (new PropertyResource($property))
@@ -82,20 +114,26 @@ class PropertyController extends Controller
         return response()->json(null, 204); //no content
     }
 
-    public function review(ReviewRequest $request){
+     /**
+     * Store a newly created review in storage.
+     * @bodyParam message string[] required message of the review. Example: ["This review is super beautiful"]
+     * @bodyParam rating int[] Example: 5
+     * @bodyParam author_id int required The author id of the review. Example: 1
+     * @bodyParam property_id int required The property id that the review belongs to. Example: 1
+     * @apiResource App\Http\Resources\v1/ReviewResource
+     * @apiResourceModel App\Models\Review
+     * @param \Illuminate\Http\Request $request
+     * @return ReviewResource
+     */
+    public function review(ReviewRequest $request, ReviewRepository $repository){
         
-        $review = Review::create([
-            'rating' => $request->rating,
-            'message' => $request->message,
-            'property_id' => $request->property_id,
-            'author_id' => auth()->id(),
-        ]);
+        $review = $repository->create($request->only([
+            'message',
+            'rating',
+            'property_id',
+        ]));
 
-        event(new ReviewCreated($review));
-
-        return (new ReviewResource($review))
-        ->response()->json()
-        ->setStatusCode(201);
+        return (new ReviewResource($review));
 
     }
 }
--- app/Listeners/SendNewBookingListener.php
+++ app/Listeners/SendNewBookingListener.php
@@ -17,4 +17,3 @@ class SendNewBookingListener
 
     }
 }
-send
